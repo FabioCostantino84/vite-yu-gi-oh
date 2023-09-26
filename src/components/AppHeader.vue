6bd89b4bfed668119e3caf43f7c1ffc4851c190c
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-26 17:14:59 +0200

    add AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <div class="d-flex align-items-center gap-2 ms-2">
+            <img width="100" src="../assets/img/yu-gi-oh-logo.png.png" alt="">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
