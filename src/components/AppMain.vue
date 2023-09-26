5b79e0f07508cdf847ca00c0a4211b55088a4acc
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-26 17:50:06 +0200

    add AppMain and v-for

---
+++ src/components/AppMain.vue
@@ -0,0 +1,44 @@
+<script>
+import { store } from '../store.js'
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            store
+        }
+    },
+    created() {
+        store.fetchData();
+    }
+}
+</script>
+    
+<template>
+
+    
+    <main class="py-4">
+        <div class="container bg-white">
+            <div class="row p-2">
+                <div class="col-3 p-1" v-for="card in store.cards">
+                    <div class="card" style="width: 18rem;">
+                        <img class="w-100" :src='card.card_images[0].image_url' alt="">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { reactive } from 'vue';
+
+export const store = reactive({
+    base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0',
+    cards: null,
+    info: null,
+
+    fetchData() {
+        axios(this.base_url)
+            .then(response => {
+                console.log(response)
+                console.log(response.data);
+                console.log(response.data.data);
+
+
+                this.cards = response.data.data;
+
+            })
+    }
+})
