a02bfed556fefbe599e14a7b218d5c6372ec254b
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-26 19:01:17 +0200

    add main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,17 +19,16 @@ export default {
     
     <main class="py-4">
         <div class="container bg-white">
+            <div class="container counterCard text-white">Founded 50</div>
             <div class="row p-2">
-                <div class="col-3 p-1" v-for="card in store.cards">
-                    <div class="card" style="width: 18rem;">
+                <div class="my_col p-2" v-for="card in store.cards">
+                    <div class="card">
                         <img class="w-100" :src='card.card_images[0].image_url' alt="">
-                      <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <div style="height: 120px;" class="card-body ">
+                        <h5 class="card-title text-uppercase text-align-center">{{ card.name }}</h5>
+                        <p class="card-text">{{ card.archetype }}</p>
                       </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
@@ -40,5 +39,17 @@ export default {
 
 
 <style lang="scss" scoped>
+
+main{
+    background-color: orange;
+}
+
+.counterCard{
+    background-color: black;
+    height: 50px;
+}
+.my_col{
+    width: calc(100% / 10 * 2);
+}
 
 </style>
