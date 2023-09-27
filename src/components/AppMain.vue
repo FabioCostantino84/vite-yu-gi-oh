82a856afc062575c803fe0301d9ae1732e0a4dc0
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-27 12:31:37 +0200

    add select archetype

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,10 +20,12 @@ export default {
         <div class="container">
 
             <select class="form-select mb-4 fs-4" aria-label="">
-                <option selected>Alien</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option selected disabled>Select Archetype</option>
+                <option value="1">Alien</option>
+                <option value="1">Noble Knight</option>
+                <option value="2">Tainted Treasure</option>
+                <option value="3">Melodious</option>
+                <option value="3">Archfiend</option>
             </select>
 
         </div>
